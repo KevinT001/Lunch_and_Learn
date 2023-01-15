f33e3790ff3beb14fd9b4f9a65fa9ba837b5b5b9
Kevin Ta <kvnta@yahoo.com>
2023-01-15 02:45:52 -0700

    country_service_spec written

---
+++ spec/services/country_service_spec.rb
@@ -0,0 +1,14 @@
+require 'rails_helper'
+
+RSpec.describe 'CountryService' do 
+
+  it '.retrieve_all_countries' do 
+    countries = CountryService.retrieve_all_countries
+    first_country = countries.first
+
+    expect(countries).to be_a Array
+    expect(first_country).to have_key(:name)
+    expect(first_country[:name]).to have_key(:common)
+    expect(first_country[:name][:common]).to be_a(String)
+  end
+end
